9322dc3400ff42172a43b327db4f19829e5970fa
mddo <mddo>
2023-05-15 07:20:54 +0000

    update demo scripts

--- demo/copy_to_emulated_env/demo_step2.sh
+++ demo/copy_to_emulated_env/demo_step2.sh
@@ -13,5 +13,5 @@ curl -s -X POST -H 'Content-Type: application/json' \
 
 ansible-runner run . -p /data/project/playbooks/step02.yml --container-option="--net=${NODERED_BRIDGE}" \
 	--container-volume-mount="$PWD:/data" --container-image=${ANSIBLERUNNER_IMAGE} \
-       	--process-isolation --process-isolation-executable docker --cmdline  \
-	"-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e labname=${LABNAME} -e login_user=${LOCALSERVER_USER} -e network_name=${NETWORK_NAME} -e demo_dir=${DEMO_DIR} -k -K " -vvvv                                                                  
+       	--process-isolation --process-isolation-executable docker --cmdline \
+        "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e login_user=${LOCALSERVER_USER} -k -K " -vvvv 
