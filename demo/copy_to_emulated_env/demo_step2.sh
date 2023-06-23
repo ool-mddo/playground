e4e844eea45e3497e55fd331a1c832077becf3e0
t-tkgh <you@example.com>
2023-06-23 14:53:27 +0900

    update step2

--- demo/copy_to_emulated_env/demo_step2.sh
+++ demo/copy_to_emulated_env/demo_step2.sh
@@ -19,7 +19,7 @@ curl -s -X POST -H 'Content-Type: application/json' \
 ansible-runner run . -p /data/project/playbooks/step2.yaml --container-option="--net=${API_BRIDGE}" \
   --container-volume-mount="$PWD:/data" --container-image="${ANSIBLERUNNER_IMAGE}" \
   --process-isolation --process-isolation-executable docker \
-  --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e login_user=${LOCALSERVER_USER} -k -K " -vvvv
+  --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e login_user=${LOCALSERVER_USER} -k -K " 
 
 # update netoviz index
 curl -s -X POST -H 'Content-Type: application/json' \
