cc102ae84610ecef79313ba3c1e9bd029c0b4b24
m.hagiwara <manabu.hagiwara@okinawaopenlabs.org>
2023-10-27 13:01:20 +0900

    V1.3.1 dev (#35)
    * Update env
    
    * Update step1.5/2 task assignation
    
    * Update step1.5 for ttp dir path update
    
    * Update components

--- demo/copy_to_emulated_env/demo_step1.5.sh
+++ demo/copy_to_emulated_env/demo_step1.5.sh
@@ -9,7 +9,19 @@ exec_parser="docker compose exec bgp-policy-parser"
 $exec_parser python collect_configs.py -n "$NETWORK_NAME"
 
 # parse configuration files with TTP
-$exec_parser python main.py
+$exec_parser python main.py -n "$NETWORK_NAME"
 
 # post bgp policy data to model-conductor to merge it to topology data
 $exec_parser python post_bgp_policies.py -n "$NETWORK_NAME"
+
+BIGLOBE_NETWORK_PATTERN="^biglobe.*$"
+if [[ "$NETWORK_NAME" =~ $BIGLOBE_NETWORK_PATTERN ]]; then
+  # generate external-AS topology
+  external_as_json="${NETWORK_NAME}_ext.json"
+  curl -s "http://${API_PROXY}/topologies/${NETWORK_NAME}/original_asis/external_as_topology" > "$external_as_json"
+  # splice external-AS topology to original_asis (overwrite)
+  curl -s -X POST -H "Content-Type: application/json" \
+    -d @<(jq '{ "overwrite": true, "ext_topology_data": . }' "$external_as_json") \
+    "http://${API_PROXY}/conduct/${NETWORK_NAME}/original_asis/splice_topology" \
+    > /dev/null # ignore echo-back (topology json)
+fi
