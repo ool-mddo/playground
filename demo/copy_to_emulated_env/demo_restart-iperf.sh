60d8635ffeb4f655e038e440c1ca9ed8ee62bf92
mddo <mddo>
2023-12-11 11:10:24 +0000

    update docker compose network

--- demo/copy_to_emulated_env/demo_restart-iperf.sh
+++ demo/copy_to_emulated_env/demo_restart-iperf.sh
@@ -30,5 +30,5 @@ if "${WITH_CLAB:-true}"; then
   ansible-runner run . -p /data/project/playbooks/restart-iperf.yaml --container-option="--net=${API_BRIDGE}" \
     --container-volume-mount="$PWD:/data" --container-image="${ANSIBLERUNNER_IMAGE}" \
     --process-isolation --process-isolation-executable docker \
-    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e login_user=${LOCALSERVER_USER} -e network_name=${NETWORK_NAME} -k -K "
+    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e playground_dir=${PLAYGROUND_DIR} -e login_user=${LOCALSERVER_USER} -e network_name=${NETWORK_NAME} -k -K "
 fi
