fec8beb4547e45965a097a93af3a8908af0abe8d
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2024-04-24 19:31:52 +0900

    Reconstruct file path, step2-1/2-2, demo scripts

--- demo/copy_to_emulated_env/demo_step2-2.sh
+++ demo/copy_to_emulated_env/demo_step2-2.sh
@@ -30,5 +30,5 @@ if "${WITH_CLAB:-true}"; then
   ansible-runner run . -p /data/project/playbooks/step2-2.yaml --container-option="--net=${API_BRIDGE}" \
     --container-volume-mount="$PWD:/data" --container-image="${ANSIBLE_RUNNER_IMAGE}" \
     --process-isolation --process-isolation-executable docker \
-    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e playground_dir=${PLAYGROUND_DIR} -e login_user=${LOCALSERVER_USER} -e network_name=${NETWORK_NAME} -k -K "
+    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e login_user=${LOCALSERVER_USER} -e network_name=${NETWORK_NAME} -e usecase_name=${USECASE_NAME} -e usecase_common_name=${USECASE_COMMON_NAME} -k -K "
 fi
--- demo/copy_to_emulated_env/project/playbooks/pni/restart-iperf.sh
+++ demo/copy_to_emulated_env/project/playbooks/pni/restart-iperf.sh
@@ -7,6 +7,13 @@
 ###"^SSH password:\\s*?$": "login password"
 ###"^BECOME password.*:\\s*?$": "login password"
 
+current_dir=$PWD
+script_dir=$(dirname "$0")
+exec_dir="$script_dir/../../.." # copy_to_emulated_env dir
+
+cd "$exec_dir" || exit 1
+echo "# change dir: $current_dir -> $PWD"
+
 # shellcheck disable=SC1091
 source ./demo_vars
 
@@ -21,14 +28,19 @@ while getopts d option; do
     ;;
   *)
     echo "Unknown option detected, $option"
+    cd "$current_dir"
     exit 1
   esac
 done
 
 # configure iperf client/server
 if "${WITH_CLAB:-true}"; then
-  ansible-runner run . -p /data/project/playbooks/restart-iperf.yaml --container-option="--net=${API_BRIDGE}" \
+  ansible-runner run . -p "/data/project/playbooks/${USECASE_COMMON_NAME}/restart-iperf.yaml" --container-option="--net=${API_BRIDGE}" \
     --container-volume-mount="$PWD:/data" --container-image="${ANSIBLE_RUNNER_IMAGE}" \
     --process-isolation --process-isolation-executable docker \
-    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e playground_dir=${PLAYGROUND_DIR} -e login_user=${LOCALSERVER_USER} -e network_name=${NETWORK_NAME} -k -K "
+    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -e playground_dir=${PLAYGROUND_DIR} -e login_user=${LOCALSERVER_USER} -e network_name=${NETWORK_NAME} -e usecase_name=${USECASE_NAME} -e usecase_common_name=${USECASE_COMMON_NAME} -k -K "
 fi
+
+echo "# return dir: $PWD -> $current_dir"
+cd "$current_dir"
+
