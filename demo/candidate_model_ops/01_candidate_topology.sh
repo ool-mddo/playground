2deb7b32806df29ad700dfb72da0a9ca58f03314
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2024-07-25 10:35:43 +0900

    add scenario scripts

---
+++ demo/candidate_model_ops/01_candidate_topology.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+
+# shellcheck disable=SC1091
+source ./demo_vars
+
+# original as-is Create topology data
+curl -s -X DELETE "http://${API_PROXY}/conduct/${NETWORK_NAME}"
+curl -s -X POST -H 'Content-Type: application/json' \
+  -d '{ "label": "OSPF model (original_asis)", "phy_ss_only": true }' \
+  "http://${API_PROXY}/conduct/${NETWORK_NAME}/original_asis/topology"
+
+if use_bgp_proc "$NETWORK_NAME" original_asis ; then
+  echo "Network:$NETWORK_NAME uses BGP, expand external-AS network and splice it into topology data"
+
+  # bgp-policy data handling
+  # parse configuration files with TTP
+  curl -s -X POST -H "Content-Type: application/json" \
+    -d '{}' \
+    "http://${API_PROXY}/bgp_policy/${NETWORK_NAME}/original_asis/parsed_result"
+
+  # post bgp policy data to model-conductor to merge it with topology data
+  curl -s -X POST -H "Content-Type: application/json" \
+    -d '{}' \
+    "http://${API_PROXY}/bgp_policy/${NETWORK_NAME}/original_asis/topology"
+
+  # generate external-AS topology
+  external_as_topology_dir="${USECASE_COMMON_DIR}/external_as_topology"
+  if [ ! -e "$external_as_topology_dir" ]; then
+    external_as_topology_dir="${USECASE_DIR}/external_as_topology"
+  fi
+  external_as_script="${external_as_topology_dir}/main.rb"
+  external_as_json="${USECASE_CONFIGS_DIR}/external_as_topology.json"
+  params_file="${USECASE_DIR}/params.yaml"
+  flowdata_file="${USECASE_DIR}/flowdata.csv"
+  ruby "$external_as_script" -p "$params_file" -f "$flowdata_file"  > "$external_as_json"
+  # splice external-AS topology to original_asis (overwrite)
+  curl -s -X POST -H "Content-Type: application/json" \
+    -d @<(jq '{ "overwrite": true, "ext_topology_data": . }' "$external_as_json") \
+    "http://${API_PROXY}/conduct/${NETWORK_NAME}/original_asis/splice_topology" \
+    > /dev/null # ignore echo-back (topology json)
+fi
+
+# Generate candidate configs
+original_candidate_list="${USECASE_CONFIGS_DIR}/original_candidate_list.json"
+curl -s -X POST -H 'Content-Type: application/json' \
+  -d '{ "candidate_number": "'"$CANDIDATE_NUM"'", "usecase": "'"$USECASE_NAME"'"}' \
+  "http://${API_PROXY}/conduct/${NETWORK_NAME}/original_asis/candidate_topology" \
+  > "$original_candidate_list"
+
+# Add netoviz index
+netoviz_original_asis_index="${USECASE_CONFIGS_DIR}/netoviz_original_asis_index.json"
+jq '.[0:1]' "$NETWORK_INDEX" > "$netoviz_original_asis_index"
+netoviz_original_candidates_index="${USECASE_CONFIGS_DIR}/netoviz_original_candidates_index.json"
+filter='map(. + {label: ( "\(.network | ascii_upcase) (\(.snapshot))"), file: "topology.json"})'
+jq "$filter" "$original_candidate_list" > "$netoviz_original_candidates_index"
+netoviz_index="${USECASE_CONFIGS_DIR}/netoviz_index.json"
+jq -s '.[0] + .[1]' "$netoviz_original_asis_index" "$netoviz_original_candidates_index" > "$netoviz_index"
+
+curl -s -X POST -H 'Content-Type: application/json' \
+  -d @<(jq '{ "index_data": . }' "$netoviz_index") \
+  "http://${API_PROXY}/topologies/index"
---
+++ demo/candidate_model_ops/02_candidate_config.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/bash
+
+# shellcheck disable=SC1091
+source ./demo_vars
+
+print_usage() {
+  echo "Usage: $(basename "$0") [options]"
+  echo "Options:"
+  echo "  -d     Debug/data check, without executing ansible-runner (clab)"
+  echo "  -h     Display this help message"
+}
+
+# option check
+# defaults
+WITH_CLAB=true
+while getopts dh option; do
+  case $option in
+  d)
+    # data check, debug
+    # -> without container lab; does not build emulated-env
+    WITH_CLAB=false
+    echo "# Check: with_clab = $WITH_CLAB"
+    ;;
+  h)
+    print_usage
+    exit 0
+    ;;
+  *)
+    echo "Unknown option detected, -$OPTARG" >&2
+    print_usage
+    exit 1
+    ;;
+  esac
+done
+
+original_candidate_list="${USECASE_CONFIGS_DIR}/original_candidate_list.json"
+for original_candidate_snapshot in $(jq -r ".[] | .snapshot" "$original_candidate_list")
+do
+  echo "Target snapshot: $original_candidate_snapshot"
+
+  # convert namespace from original_candidate_i to emulated_candidate_i
+  emulated_candidate_snapshot="${original_candidate_snapshot/original/emulated}"
+  echo "Convert to: $emulated_candidate_snapshot"
+  curl -s -X POST -H 'Content-Type: application/json' \
+    -d '{ "table_origin": "'"$original_candidate_snapshot"'" }' \
+    "http://${API_PROXY}/conduct/${NETWORK_NAME}/ns_convert/original_asis/${emulated_candidate_snapshot}"
+
+  # generate emulated_candidate configs from emulated_candidate topology
+  echo "# TODO: exec ansible to generate $emulated_candidate_snapshot"
+  # ansible-runner run . -p /data/project/playbooks/step2-1.yaml \
+  #   --container-option="--net=${API_BRIDGE}" \
+  #   --container-image="${ANSIBLE_RUNNER_IMAGE}" \
+  #   --container-volume-mount="$PWD:/data" \
+  #   --process-isolation \
+  #   --process-isolation-executable docker \
+  #   --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} \
+  #             -e login_user=${LOCALSERVER_USER} \
+  #             -e network_name=${NETWORK_NAME} \
+  #             -e crpd_image=${CRPD_IMAGE} \
+  #             -e with_clab=${WITH_CLAB} \
+  #             -k -K"
+done
+
+# update netoviz index
+netoviz_original_asis_index="${USECASE_CONFIGS_DIR}/netoviz_original_asis_index.json"
+netoviz_original_candidates_index="${USECASE_CONFIGS_DIR}/netoviz_original_candidates_index.json"
+netoviz_emulated_candidate_index="${USECASE_CONFIGS_DIR}/netoviz_emulated_candidate_index.json"
+filter='map(.snapshot |= sub("original"; "emulated") | . + {label: ("MDDO-BGP (" + .snapshot + ")"), file: "topology.json"})'
+jq "$filter" "$original_candidate_list" > "$netoviz_emulated_candidate_index"
+netoviz_index="${USECASE_CONFIGS_DIR}/netoviz_index.json"
+jq -s '.[0] + .[1] + .[2]' "$netoviz_original_asis_index" "$netoviz_original_candidates_index" "$netoviz_emulated_candidate_index" \
+  > "$netoviz_index"
+
+curl -s -X POST -H 'Content-Type: application/json' \
+  -d @<(jq '{ "index_data": . }' "$netoviz_index") \
+  "http://${API_PROXY}/topologies/index"
