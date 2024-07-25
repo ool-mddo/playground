ed142a804943297b8b27f661ca204771bcbc7a7e
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2024-07-25 14:23:36 +0900

    Update playbooks

--- demo/candidate_model_ops/up_emulated_env.sh
+++ demo/candidate_model_ops/up_emulated_env.sh
@@ -17,35 +17,37 @@ function up_emulated_env() {
 
   # generate emulated_candidate configs from emulated_candidate topology
   echo "Exec ansible to generate $target_emulated_snapshot configs"
-  # ansible-runner run . -p /data/project/playbooks/step2-1.yaml \
-  #   --container-option="--net=${API_BRIDGE}" \
-  #   --container-image="${ANSIBLE_RUNNER_IMAGE}" \
-  #   --container-volume-mount="$PWD:/data" \
-  #   --process-isolation \
-  #   --process-isolation-executable docker \
-  #   --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} \
-  #             -e login_user=${LOCALSERVER_USER} \
-  #             -e network_name=${NETWORK_NAME} \
-  #             -e crpd_image=${CRPD_IMAGE} \
-  #             -e with_clab=${WITH_CLAB} \
-  #             -k -K"
+  ansible-runner run . -p /data/project/playbooks/step2-1.yaml \
+    --container-option="--net=${API_BRIDGE}" \
+    --container-image="${ANSIBLE_RUNNER_IMAGE}" \
+    --container-volume-mount="$PWD:/data" \
+    --process-isolation \
+    --process-isolation-executable docker \
+    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} \
+              -e login_user=${LOCALSERVER_USER} \
+              -e network_name=${NETWORK_NAME} \
+              -e snapshot_name=${target_emulated_snapshot} \
+              -e crpd_image=${CRPD_IMAGE} \
+              -e with_clab=${WITH_CLAB} \
+              -k -K"
 
   # generate emulated_candidate environment from emulated_candidate topology/configs
-  echo "# TODO: exec ansible to generate $target_emulated_snapshot clab env"
-  # ansible-runner run . -p "/data/project/playbooks/step2-2.yaml" \
-  #   --container-option="--net=${API_BRIDGE}" \
-  #   --container-image="${ANSIBLE_RUNNER_IMAGE}" \
-  #   --container-volume-mount="$PWD:/data" \
-  #   --process-isolation \
-  #   --process-isolation-executable docker \
-  #   --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} \
-  #             -e login_user=${LOCALSERVER_USER} \
-  #             -e network_name=${NETWORK_NAME} \
-  #             -e usecase_name=${USECASE_NAME} \
-  #             -e usecase_common_name=${USECASE_COMMON_NAME} \
-  #             -e with_clab=${WITH_CLAB} \
-  #             -e clab_restart=${CLAB_RESTART} \
-  #             -k -K"
+  echo "# Exec ansible to generate $target_emulated_snapshot clab env"
+  ansible-runner run . -p "/data/project/playbooks/step2-2.yaml" \
+    --container-option="--net=${API_BRIDGE}" \
+    --container-image="${ANSIBLE_RUNNER_IMAGE}" \
+    --container-volume-mount="$PWD:/data" \
+    --process-isolation \
+    --process-isolation-executable docker \
+    --cmdline "-e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} \
+              -e login_user=${LOCALSERVER_USER} \
+              -e network_name=${NETWORK_NAME} \
+              -e snapshot_name=${target_emulated_snapshot} \
+              -e usecase_name=${USECASE_NAME} \
+              -e usecase_common_name=${USECASE_COMMON_NAME} \
+              -e with_clab=${WITH_CLAB} \
+              -e clab_restart=false \
+              -k -K"
 
   ###############
   # satate part #
