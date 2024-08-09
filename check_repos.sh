78c87c52cee4292da1cc267fe7d3e5f2f7eb4bdb
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2024-08-09 14:56:12 +0900

    Add current_commit column to check_repos.sh

--- check_repos.sh
+++ check_repos.sh
@@ -29,23 +29,25 @@ function is_branch_up_to_date () {
 
 function output_playground_csv () {
     repository_name="playground"
+    target_branch="NONE"
     current_branch=$(git branch --show-current)
     current_tag=$(git describe --tags --exact-match 2>/dev/null)
-    target_branch="NONE"
+    current_commit=$(git rev-parse --short HEAD)
     is_up_to_date=$(is_branch_up_to_date "$current_branch")
-    echo "$repository_name, $current_branch, $current_tag, $target_branch, $is_up_to_date"
+    echo "$repository_name, $target_branch, $current_branch, $current_tag, $current_commit, $is_up_to_date"
 }
 
 function output_repos_csv () {
     for repo in repos/*; do
         pushd "$repo" > /dev/null || exit 2
 
+        target_branch=$(target_branch "$repo")
         current_branch=$(git branch --show-current)
         current_tag=$(git describe --tags --exact-match 2>/dev/null)
-        target_branch=$(target_branch "$repo")
+        current_commit=$(git rev-parse --short HEAD)
         is_up_to_date=$(is_branch_up_to_date "$current_branch")
         # record
-        echo "$repo, $current_branch, $current_tag, $target_branch, $is_up_to_date"
+        echo "$repo, $target_branch, $current_branch, $current_tag, $current_commit, $is_up_to_date"
 
         popd > /dev/null || exit 2
     done
@@ -59,7 +61,7 @@ function output_all_csv () {
     cd "$SCRIPT_DIR" || exit 1
 
     # header
-    echo "repository, current-branch, current-tag, target-branch/tag, up-to-date?"
+    echo "repository, target-branch/tag, current-branch, current-tag, current-commit, up-to-date?"
 
     # playground dir
     output_playground_csv "$SCRIPT_DIR"
