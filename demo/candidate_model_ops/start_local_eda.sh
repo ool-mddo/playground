5a8a85395e20e2516be645cf9822a6c242894c1b
Your Name <you@example.com>
2025-02-27 03:19:00 +0900

    update ansible-eda restart

--- demo/candidate_model_ops/start_local_eda.sh
+++ demo/candidate_model_ops/start_local_eda.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
+#!/usr/bin/bash
 
+# shellcheck disable=SC1091
+source ./demo_vars
+# shellcheck disable=SC1091
+source ./util.sh
+
+echo ${ANSIBLE_EDA_CLAB}
 # stop ansible-eda if running
-# shellcheck disable=SC2009
 eda_pid=$(ps -ef | grep /eda/rulebook.yaml | grep -v grep | awk '{print $2}')
 if [ -n "$eda_pid" ]; then
-  sudo kill -9 "$eda_pid"
+  curl -H "Content-Type: application/json" \
+    -d '{
+          "message": "shutdown"
+        }' \
+    "http://${ANSIBLE_EDA_CLAB}/endpoint"
 fi
 
+
+
 echo 'job_status{jobname="",network_name="",snapshot_name=""} 0' > ./node_exporter/prom/textfile.prom
-sudo docker compose -f node_exporter/docker-compose.yaml up -d
-sudo ansible-rulebook -i ../../ansible-eda/hosts --rulebook ../../ansible-eda/containerlab_rulebook.yaml -vvvv
+docker compose -f node_exporter/docker-compose.yaml up -d
+ansible-rulebook -i ../../ansible-eda/hosts --rulebook ../../ansible-eda/containerlab_rulebook.yaml -vvvv
