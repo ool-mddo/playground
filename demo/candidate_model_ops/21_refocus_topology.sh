b14e3bece1ee6c10c2f98781ee9511b2b78b6129
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2026-09-21 08:54:24 +0900

    Refactoring: 21_refocus_topology.sh

--- demo/candidate_model_ops/21_refocus_topology.sh
+++ demo/candidate_model_ops/21_refocus_topology.sh
@@ -7,6 +7,17 @@ source ./orig_ns_topology.sh
 
 echo # newline
 
+# Append snapshot entries (JSON array) to the current netoviz index
+append_netoviz_entries() {
+  local new_entries="$1"
+  local current_index
+  current_index=$(curl -s "http://${API_PROXY}/topologies/index")
+  local updated_index
+  updated_index=$(jq -n --argjson curr "${current_index}" --argjson new "${new_entries}" '$curr + $new')
+  echo "{\"index_data\": ${updated_index}}" | \
+    curl -s -X POST -H "Content-Type: application/json" -d@- "http://${API_PROXY}/topologies/index"
+}
+
 # clear all snapshots
 curl -X DELETE "http://${API_PROXY}/topologies/${NETWORK_NAME}"
 
@@ -28,42 +39,28 @@ echo # newline
 conduit_response=$(generate_conduit_topology "${NETWORK_NAME}" "original_asis" "${USECASE_NAME}" "original_asis_blueprint")
 echo "# Conduit response: ${conduit_response}"
 
-# Append conduit snapshot entries to current netoviz index
-current_index=$(curl -s "http://${API_PROXY}/topologies/index")
-new_entries=$(echo "${conduit_response}" | jq --arg nw "${NETWORK_NAME}" \
+# Append original_asis_conduit* entries to current netoviz index
+original_conduit_entries=$(echo "${conduit_response}" | jq --arg nw "${NETWORK_NAME}" \
   '[.[] | {
     "label": (($nw | ascii_upcase) + " (" + .snapshot + ")"),
     "network": $nw,
     "snapshot": .snapshot,
     "file": "topology.json"
   }]')
-updated_index=$(jq -n --argjson curr "${current_index}" --argjson new "${new_entries}" '$curr + $new')
-echo "{\"index_data\": ${updated_index}}" | \
-  curl -s -X POST -H "Content-Type: application/json" -d@- "http://${API_PROXY}/topologies/index"
+append_netoviz_entries "${original_conduit_entries}"
 
 echo # newline
 
-# Convert namespace: original_asis -> emulated_asis
+# Convert namespace: original_asis -> emulated_asis, then add emulated_asis to netoviz index
 convert_namespace "original_asis"
+append_netoviz_entries "$(jq -n --arg nw "${NETWORK_NAME}" \
+  '[{"label": (($nw | ascii_upcase) + " (emulated_asis)"), "network": $nw, "snapshot": "emulated_asis", "file": "topology.json"}]')"
 
-# Convert namespace for each conduit snapshot: original_asis_conduit<X> -> emulated_asis_conduit<X>
+# Convert namespace for each conduit snapshot and append each emulated entry to netoviz index
 for conduit_snapshot in $(echo "${conduit_response}" | jq -r '.[] | .snapshot'); do
   convert_namespace "${conduit_snapshot}"
+  emulated_snapshot=$(reverse_snapshot_name "${conduit_snapshot}")
+  append_netoviz_entries "$(jq -n --arg nw "${NETWORK_NAME}" --arg snap "${emulated_snapshot}" \
+    '[{"label": (($nw | ascii_upcase) + " (" + $snap + ")"), "network": $nw, "snapshot": $snap, "file": "topology.json"}]')"
 done
-
-# Build emulated_asis* entries for netoviz index (emulated_asis + emulated_asis_conduit<X>)
-emulated_entries=$(echo "${conduit_response}" | jq --arg nw "${NETWORK_NAME}" \
-  '[{"label": (($nw | ascii_upcase) + " (emulated_asis)"), "network": $nw, "snapshot": "emulated_asis", "file": "topology.json"}] +
-   [.[] | {
-     "label": (($nw | ascii_upcase) + " (" + (.snapshot | gsub("^original"; "emulated")) + ")"),
-     "network": $nw,
-     "snapshot": (.snapshot | gsub("^original"; "emulated")),
-     "file": "topology.json"
-   }]')
-
-# Append emulated_asis* entries to current netoviz index
-current_index=$(curl -s "http://${API_PROXY}/topologies/index")
-updated_index=$(jq -n --argjson curr "${current_index}" --argjson new "${emulated_entries}" '$curr + $new')
-echo "{\"index_data\": ${updated_index}}" | \
-  curl -s -X POST -H "Content-Type: application/json" -d@- "http://${API_PROXY}/topologies/index"
 
