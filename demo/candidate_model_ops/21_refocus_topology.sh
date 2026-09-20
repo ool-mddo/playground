f3442b1132030858a4e5a374ff45194e3e9368c1
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2026-09-20 09:05:07 +0900

    move function generate_conduit_topology to orig_ns_topology.sh

--- demo/candidate_model_ops/21_refocus_topology.sh
+++ demo/candidate_model_ops/21_refocus_topology.sh
@@ -24,35 +24,20 @@ curl -X POST -H "Content-Type: application/json" -d@- "http://${API_PROXY}/topol
 
 echo # newline
 
-# Generate conduit topologies and update netoviz index
-generate_conduit_topology() {
-  local network=$1
-  local snapshot=$2           # e.g. "original_asis"
-  local usecase=$3            # e.g. "refocus_topology"
-  local blueprint_snapshot=$4 # e.g. "original_asis_blueprint"
+# Generate conduit topologies
+conduit_response=$(generate_conduit_topology "${NETWORK_NAME}" "original_asis" "${USECASE_NAME}" "original_asis_blueprint")
+echo "# Conduit response: ${conduit_response}"
 
-  echo "# Generate conduit topology: ${network}/${snapshot} (blueprint: ${blueprint_snapshot})"
+# Append conduit snapshot entries to current netoviz index
+current_index=$(curl -s "http://${API_PROXY}/topologies/index")
+new_entries=$(echo "${conduit_response}" | jq --arg nw "${NETWORK_NAME}" \
+  '[.[] | {
+    "label": (($nw | ascii_upcase) + " (" + .snapshot + ")"),
+    "network": $nw,
+    "snapshot": .snapshot,
+    "file": "topology.json"
+  }]')
+updated_index=$(jq -n --argjson curr "${current_index}" --argjson new "${new_entries}" '$curr + $new')
+echo "{\"index_data\": ${updated_index}}" | \
+  curl -s -X POST -H "Content-Type: application/json" -d@- "http://${API_PROXY}/topologies/index"
 
-  conduit_response=$(curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -d "{\"usecase\": \"${usecase}\", \"blueprint_snapshot\": \"${blueprint_snapshot}\"}" \
-    "http://${API_PROXY}/conduct/${network}/${snapshot}/conduit_topology")
-
-  echo "# Conduit response: ${conduit_response}"
-
-  # Append conduit snapshot entries to current netoviz index
-  current_index=$(curl -s "http://${API_PROXY}/topologies/index")
-  new_entries=$(echo "${conduit_response}" | jq --arg nw "${network}" \
-    '[.[] | {
-      "label": (($nw | ascii_upcase) + " (" + .snapshot + ")"),
-      "network": $nw,
-      "snapshot": .snapshot,
-      "file": "topology.json"
-    }]')
-  updated_index=$(jq -n --argjson curr "${current_index}" --argjson new "${new_entries}" '$curr + $new')
-
-  echo "{\"index_data\": ${updated_index}}" | \
-    curl -s -X POST -H "Content-Type: application/json" -d@- "http://${API_PROXY}/topologies/index"
-}
-
-generate_conduit_topology "${NETWORK_NAME}" "original_asis" "${USECASE_NAME}" "original_asis_blueprint"
